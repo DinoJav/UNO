3ed7eb7bf0739fba1270f6e293e317dfbcd4be92
DinoJav <javmold15@gmail.com>
2023-06-14 23:05:22 -0500

    ejercicio factorial solo por consola

---
+++ factorial.js
@@ -0,0 +1,7 @@
+let factorial = 5;
+let resultado = factorial;
+for(let i = 1 ; factorial > i ; i++){
+    resultado = resultado * i 
+    console.log(`${resultado} = ${resultado} * ${i}`);
+}
+console.log(resultado);
