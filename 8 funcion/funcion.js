d8c876d5da34aa8404ff1716b3e15b31b8188f83
DinoJav <javmold15@gmail.com>
2023-06-19 23:47:19 -0500

    dando estilos y validaciones al ejercicio funcion

--- 8 funcion/funcion.js
+++ 8 funcion/funcion.js
@@ -1,29 +1,32 @@
 //EN PANTALLA CON BOTON Y UN INPUT
 
 enviarValor = () => {
-    let numero = document.getElementById('num').value;
-    let resultado = document.getElementById('resultado');
-    numeroNuevo = numero % 2;
-    if( numeroNuevo == 0 ){
-        resultado.innerHTML =  numero + ": es un número par";  
-
-    }else{
-        resultado.innerHTML =  numero + ": es un número impar";  
+  let numero = document.getElementById("num").value;
+  let resultado = document.getElementById("resultado");
+  numeroNuevo = numero % 2;
+  if (numero == "") {
+    resultado.innerHTML = "no se ingreso ningun número";
+  } else if (numero >= 0) {
+    if (numeroNuevo == 0) {
+      resultado.innerHTML = numero + ": es un número par";
+    } else {
+      resultado.innerHTML = numero + ": es un número impar";
     }
-
-}
+  } else {
+    resultado.innerHTML = numero + ": es un número negativo";
+  }
+};
 
 //POR CONSOLA
 
-enviarValorConsola = ( numero) => {
-    numeroNuevo = numero % 2;
-    if( numeroNuevo == 0 ){
-        console.log(`${numero} el número es par`)
-    }else{
-        console.log(`${numero} el número es impar`)
-    }
-
-}
+enviarValorConsola = (numero) => {
+  numeroNuevo = numero % 2;
+  if (numeroNuevo == 0) {
+    console.log(`${numero} el número es par`);
+  } else {
+    console.log(`${numero} el número es impar`);
+  }
+};
 
 enviarValorConsola(2);
 enviarValorConsola(1);
